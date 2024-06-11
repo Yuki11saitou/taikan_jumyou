63340019e1018042ec054eb8125774fb559f7dfc
Yuki11saitou <3110yuki0725@gmail.com>
2024-06-12 03:11:49 +0900

    体感寿命を算出するためMathモジュール導入

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,11 +1,20 @@
 class ResultsController < ApplicationController
   def result
+    if params[:current_age].nil? || params[:final_age].nil? || params[:current_age].to_i.zero? || params[:final_age].to_i.zero?
+      # パラメータがない場合はトップページにリダイレクトします
+      redirect_to root_path
+      return
+    end
     current_age = params[:current_age].to_i
-    expected_age = params[:expected_age].to_i
+    final_age = params[:final_age].to_i
     # 計算ロジックをここに追加します
-    @remaining_years = expected_age - current_age
+    @lifespan = final_age - current_age
+    # @lifespan_progress_rate = (current_age / final_age) * 100
+
+    @perceived_lifespan = current_age * (Math.log(final_age / current_age))
+    # @perceived_lifespan_progress_rate = ( current_age / final_age ) * 100
 
     # 結果を表示するビューをレンダリングします
-
+    render "result"
   end
 end
