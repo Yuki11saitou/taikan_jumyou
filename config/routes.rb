afe304ef7c1e6c5c88bf22ddcd4023c19e793df9
Yuki11saitou <3110yuki0725@gmail.com>
2024-06-12 00:46:18 +0900

    トップページの表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#top'
+  get 'top', to: 'static_pages#top'
+  get 'jumyou', to: 'jumyou#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
