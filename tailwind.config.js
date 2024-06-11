1e93a62fdafe9a12bd914b899ad16cb7de19b4a2
Yuki11saitou <3110yuki0725@gmail.com>
2024-06-12 00:45:16 +0900

    参考コード添付

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,26 @@ module.exports = {
     './app/javascript/**/*.js'
   ]
 }
+
+// // もちさんのファイル参考
+// const { default: daisyui } = require("daisyui");
+
+// module.exports = {
+//   content: [
+//     './app/views/**/*.html.erb',
+//     './app/helpers/**/*.rb',
+//     './app/assets/stylesheets/**/*.css',
+//     './app/javascript/**/*.js'
+//   ],
+//   theme: {
+//     extend: {},
+//       fontFamily: {
+//         'body-jp': ['Kiwi Maru', 'serif'],
+//         'body-en': ['Shrikhand', 'serif']
+//       },
+//   },
+//   plugins: [require("daisyui")],
+//   daisyui: {
+//     themes: ["light", "dark", "pastel"],
+//   },
+// }
