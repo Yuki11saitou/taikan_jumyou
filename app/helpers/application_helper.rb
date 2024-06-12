66391c16015dca77432087e1861851171acdbf21
Yuki11saitou <3110yuki0725@gmail.com>
2024-06-12 19:28:40 +0900

    計算できない値が入力された場合のフラッシュメッセージ実装の微修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def flash_background_color(type)
     case type.to_sym
-    when :notice then "bg-green-500"
-    when :alert  then "bg-red-500"
-    when :error  then "bg-yellow-500"
+    when :notice then "bg-yellow-500"
+    when :alert  then "bg-pink-500"
+    when :error  then "bg-red-500"
     else "bg-gray-500"
     end
   end
