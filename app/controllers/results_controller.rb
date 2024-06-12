90e8f4236015c3163ec3a56585a9bc8b87059618
Yuki11saitou <3110yuki0725@gmail.com>
2024-06-12 19:17:02 +0900

    計算できない値が入力された場合のフラッシュメッセージ実装

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,14 +1,18 @@
 class ResultsController < ApplicationController
   def result
-    # パラメータがない場合やゼロの場合はトップページにリダイレクト
-    if params[:current_age].nil? || params[:final_age].nil? || params[:current_age].to_i.zero? || params[:final_age].to_i.zero?
-      redirect_to root_path
+    current_age = params[:current_age].to_i.to_f
+    final_age = params[:final_age].to_i.to_f
+
+    # パラメータがゼロ、未入力の場合はトップページにリダイレクト
+    if current_age.zero? || final_age.zero? || current_age.nil? || final_age.nil?
+      redirect_to root_path, alert: "予期せぬ値が入力されました"
+      return
+    # 現在の年齢が最後の年齢よりも大きい場合はトップページにリダイレクト
+    elsif current_age > final_age
+      redirect_to root_path, notice: "現在の年齢が最後の年齢よりも大きいです"
       return
     end
 
-
-    current_age = params[:current_age].to_i.to_f
-    final_age = params[:final_age].to_i.to_f
     # 計算ロジックをここに追加します
     @lifespan = (final_age - current_age).round(1)
     # @lifespan_progress_rate = (current_age/final_age) * 100
@@ -17,6 +21,6 @@ class ResultsController < ApplicationController
     # @perceived_lifespan_progress_rate = ( current_age / final_age ) * 100
 
     # 結果を表示するビューをレンダリングします
-    render "result"
+    # render "result"
   end
 end
