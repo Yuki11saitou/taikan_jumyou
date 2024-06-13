51b076f03161acc533b33d8bde4bb8a852e59af7
Yuki11saitou <3110yuki0725@gmail.com>
2024-06-13 18:52:38 +0900

    daisyUIの導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,31 @@
-module.exports = {
-  content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
-
-// // もちさんのファイル参考
-// const { default: daisyui } = require("daisyui");
-
 // module.exports = {
 //   content: [
 //     './app/views/**/*.html.erb',
 //     './app/helpers/**/*.rb',
 //     './app/assets/stylesheets/**/*.css',
 //     './app/javascript/**/*.js'
-//   ],
-//   theme: {
-//     extend: {},
-//       fontFamily: {
-//         'body-jp': ['Kiwi Maru', 'serif'],
-//         'body-en': ['Shrikhand', 'serif']
-//       },
-//   },
-//   plugins: [require("daisyui")],
-//   daisyui: {
-//     themes: ["light", "dark", "pastel"],
-//   },
+//   ]
 // }
+
+// もちさんのファイル参考
+const { default: daisyui } = require("daisyui");
+
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {},
+      fontFamily: {
+        'body-jp': ['Kiwi Maru', 'serif'],
+        'body-en': ['Shrikhand', 'serif']
+      },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "pastel"],
+  },
+}
