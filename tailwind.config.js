123c1deca783aeb5d5f588248a3dc30fc571ce3b
Yuki11saitou <3110yuki0725@gmail.com>
2024-06-13 23:11:24 +0900

    画像とアニメーション追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,49 @@ module.exports = {
         'body-en': ['Shrikhand', 'serif']
       },
   },
+
+  theme: {
+      extend: {
+          fontFamily: {
+            'body-jp': ['Kiwi Maru', 'serif'],
+            'body-en': ['Shrikhand', 'serif']
+          },
+          // 画像のアニメーション追加
+          animation: {
+              "bounce-in-top": "bounce-in-top 1.1s ease   both"
+          },
+          keyframes: {
+              "bounce-in-top": {
+                  "0%": {
+                      transform: "translateY(-500px)",
+                      "animation-timing-function": "ease-in",
+                      opacity: "0"
+                  },
+                  "38%": {
+                      transform: "translateY(0)",
+                      "animation-timing-function": "ease-out",
+                      opacity: "1"
+                  },
+                  "55%": {
+                      transform: "translateY(-65px)",
+                      "animation-timing-function": "ease-in"
+                  },
+                  "72%,90%,to": {
+                      transform: "translateY(0)",
+                      "animation-timing-function": "ease-out"
+                  },
+                  "81%": {
+                      transform: "translateY(-28px)"
+                  },
+                  "95%": {
+                      transform: "translateY(-8px)",
+                      "animation-timing-function": "ease-in"
+                  }
+              }
+          }
+      }
+  },
+
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["light", "dark", "night"],
