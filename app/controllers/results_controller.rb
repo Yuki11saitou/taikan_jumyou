758312b092c58d5910d6b8f3224dc4a28760b6bc
Yuki11saitou <3110yuki0725@gmail.com>
2024-06-14 23:58:16 +0900

    結果ページ編集のためresultコントローラーのロジックを一時コメントアウト

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,15 +3,15 @@ class ResultsController < ApplicationController
     current_age = params[:current_age].to_i.to_f
     final_age = params[:final_age].to_i.to_f
 
-    if current_age.nil? || final_age.nil? || current_age.zero? || final_age.zero?
-      # パラメータが未入力、ゼロの場合はトップページにリダイレクト
-      redirect_to root_path, alert: "年齢、予想寿命を入力してください"
-      return
-    elsif current_age > final_age
-      # 現在の年齢が最後の年齢よりも大きい場合はトップページにリダイレクト
-      redirect_to root_path, notice: "現在の年齢が予想寿命よりも大きいです"
-      return
-    end
+    # if current_age.nil? || final_age.nil? || current_age.zero? || final_age.zero?
+    #   # パラメータが未入力、ゼロの場合はトップページにリダイレクト
+    #   redirect_to root_path, alert: "年齢、予想寿命を入力してください"
+    #   return
+    # elsif current_age > final_age
+    #   # 現在の年齢が最後の年齢よりも大きい場合はトップページにリダイレクト
+    #   redirect_to root_path, notice: "現在の年齢が予想寿命よりも大きいです"
+    #   return
+    # end
 
     # 寿命
     @lifespan = (final_age - current_age).round(1)
